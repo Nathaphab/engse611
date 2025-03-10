c923bd6f20d3ea8d8b448bf076431319e9dfd074
Nathaphab <kaewkhadcirapha@gmail.com>
2025-03-10 13:57:04 +0700

    git commit

--- lab6/challenge2/script.js
+++ lab6/challenge2/script.js
@@ -1,6 +1,8 @@
-document.querySelectorAll("#main p").forEach((p) => {
-    if (p.textContent.includes("Llamas and Chickens!")) {
-      p.style.color = "red";
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  let paragraphs = document.querySelectorAll("#main p");
+  paragraphs.forEach(paragraph => {
+      if (paragraph.textContent.includes("Llamas and Chickens!")) {
+          paragraph.style.color = "red";
+      }
   });
-  
+});
